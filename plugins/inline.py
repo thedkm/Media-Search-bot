a7d58426a81f4ce6c0aaedea631336c99e3c295b
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <16339128+thedkm@users.noreply.github.com>
2021-10-04 16:56:00 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -19,7 +19,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe channel.',
+                           switch_pm_text='You are not authorized to use this bot.',
                            switch_pm_parameter="subscribe")
         return
 
